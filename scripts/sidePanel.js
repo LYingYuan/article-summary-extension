2b149a483291abb30ee06f1f8a4ac3e4fb9a0538
Elowen Luo <74773924+LYingYuan@users.noreply.github.com>
2024-04-09 17:16:21 +0800

    feat: loading message

--- scripts/sidePanel.js
+++ scripts/sidePanel.js
@@ -2,12 +2,14 @@ document.querySelector("#article-summary__button--summarize").addEventListener("
 document.querySelector("#article-summary__button--option").addEventListener("click", openOptionsPage);
 
 function summarizeArticle() {
+  showLoading();
   chrome.runtime.sendMessage({ action: "fetchPageData" }, function (response) {
     if (response.summary) {
       document.querySelector("#article-summary__content").innerHTML = formatTextToHTML(response.summary);
     } else {
       console.error("Failed to fetch summary:", response.error);
     }
+    hideLoading();
   });
 }
 
@@ -20,4 +22,13 @@ function formatTextToHTML(text) {
   formattedText = formattedText.replace(/\n/g, `<br>`);
 
   return formattedText;
+}
+
+function showLoading() {
+  document.querySelector("#article-summary__loading").className = "article-summary__loading-visible";
+  document.querySelector("#article-summary__content").innerHTML = "";
+}
+
+function hideLoading() {
+  document.querySelector("#article-summary__loading").className = "article-summary__loading-hidden";
 }
